9e3feda54327add70c18e2cbd496d288fa12ecde
Senthil <senthil.jogi@gmail.com>
2024-01-09 22:44:03 +0100

    new example added

---
+++ 03_Target_Write_and_Read_Serial_V_1_1.py
@@ -0,0 +1,48 @@
+import chipwhisperer as cw
+import time
+
+print("Starting the Program")
+try:
+    scope = cw.scope()
+except NameError:
+    print("INFO: Unable to initialize USB connection. ?")
+
+try:
+    target = cw.target(scope)
+except IOError:
+    print("INFO: Caught exception on reconnecting to target - attempting to reconnect to scope first.")
+    print("INFO: This is a work-around when USB has died without Python knowing. Ignore errors above this line.")
+    target = cw.target(scope)
+
+print("INFO: Found ChipWhisperer😍")
+prog = cw.programmers.STM32FProgrammer
+time.sleep(0.05)
+scope.default_setup()
+
+
+print(target.baud)
+
+ktp = cw.ktp.Basic()
+key, pt = ktp.new_pair()
+print(key)
+print(pt)
+
+target.simpleserial_write('p', pt)
+ct = target.simpleserial_read('r', 16)
+print(ct)
+target.simpleserial_write('e', pt)
+ct = target.simpleserial_read('r', 16)
+print(ct)
+
+def reset_target(scope):
+    scope.io.nrst = 'low'
+    time.sleep(0.05)
+    scope.io.nrst = 'high_z'
+    time.sleep(0.05)
+
+reset_target(scope)
+
+# Debug print statement
+print("Debug: Reset target completed")
+
+
